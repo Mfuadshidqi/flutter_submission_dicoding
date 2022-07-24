ba17b2de0f91fcd3397fb14e4d34a4987e2286c4
Fuad <fuadshidqi23@gmail.com>
2022-07-24 14:53:23 +0700

    add dummy data

---
+++ lib/pahlawan.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Pahlawan{
+  String name;
+  String origin;
+  String description;
+
+  Pahlawan({required this.name,required this.origin,required this.description});
+
+}
+
+var PahlawanList = [
+  // Pahlawan(name: name, origin: origin, description: description),
+  // Pahlawan(name: name, origin: origin, description: description),
+];
